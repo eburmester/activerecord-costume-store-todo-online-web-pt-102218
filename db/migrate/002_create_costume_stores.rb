e281fac7493c0508b12c1c35622eee1943ef2324
Elyse Burmester <burmesterelyse@gmail.com>
2019-01-28 17:33:43 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,9 @@
-class CreateCostumeStores < ActiveRecordMigration[4.2]
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
   
   def change 
-    
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location 
+      t.integer :costume_inventory
+      t.integer : 
 end
