1deeed2f01774e78a8e39500dd97fbe686e385c6
Elyse Burmester <burmesterelyse@gmail.com>
2019-01-28 17:29:57 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -4,6 +4,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :name 
       t.integer :price
       t.string :size 
+      t.string :image_url
       t.datetime :created_at
       t.datetime :updated_at
     end
