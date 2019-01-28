646f4d2bb8e1f55d99876db9662059d628ecccd0
Elyse Burmester <burmesterelyse@gmail.com>
2019-01-28 17:28:30 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,8 +1,8 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change 
     create_table :costumes do |t|
-      t.string :name, 
-      t.integer :price,
+      t.string :name 
+      t.integer :price
       t.integer :size 
     end
   end
